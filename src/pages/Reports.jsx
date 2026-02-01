84267678935cc238fd84be077f5b9b9031e61630
Joshua Kitson <jkitson136@gmail.com>
2026-02-01 16:12:13 +0000

    File changes

--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -293,13 +293,9 @@ Generate a detailed, actionable report with:
                       </p>
                     </div>
                     <div className="flex gap-2">
-                      <Button variant="outline" size="sm">
-                        <Printer className="w-4 h-4 mr-2" />
-                        Print
-                      </Button>
-                      <Button variant="outline" size="sm">
+                      <Button variant="outline" size="sm" onClick={() => exportReportToPDF()}>
                         <Download className="w-4 h-4 mr-2" />
-                        Export
+                        Download PDF
                       </Button>
                     </div>
                   </div>
